7be7fe9a7610626d21edb7fc6f9d5b2868271131
srinandan <nandanks@gmail.com>
2020-06-12 14:47:02 -0700

    script to replace projects

--- change-project.sh
+++ change-project.sh
@@ -1,1 +1,17 @@
-grep -rli ./ -e '$2' *.yaml | xargs -i@ sed -i 's/$2/$2/g' @
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+   echo "requires three parameter to run: change-project mac old-value new-value"
+   exit 1
+fi
+ 
+if [ ! "$1" = "mac" -o "$1" = "lin" ]; then
+ echo "invalid type: type can be mac or lin"
+fi 
+
+if [[ "$1" == "mac" ]]
+  then     
+    find . -type f -name '*.yaml' -exec sed -i '' s/$2/$3/ {} +
+else
+  grep -rli ./ -e '$2' *.yaml | xargs -i@ sed -i 's/$2/$3/g' @
+fi
