ad2bacb64cf2d5ffd08bc91500958d50feaeb934
srinandan <nandanks@gmail.com>
2021-07-01 08:10:11 -0700

    cleanup pods

--- daily-cleanup/entrypoint.sh
+++ daily-cleanup/entrypoint.sh
@@ -14,33 +14,8 @@
 # limitations under the License.
 
 
-start_client() {
 
-  CMDSTRING="ls"
-
-  echo $CMDSTRING
-}
-
-kubectl get pods --all-namespaces -o json | jq '.items[] | select(.status.reason!=null) | select(.status.reason | contains("Evicted")) | "kubectl delete pods \(.metadata.name) -n \(.metadata.namespace)"' | xargs -n 1 bash -c
+kubectl get pods --all-namespaces -o json | jq '.items[] | select(.status.reason!=null) | select(.status.reason | contains("Evicted")) | "kubectl delete pods \(.metadata.name) -n \(.metadata.namespace)"' | xargs -n 1 bash -c && kubectl get pods --all-namespaces -o json | jq '.items[] | select(.status.reason!=null) | select(.status.reason | contains("NodeAffinity")) | "kubectl delete pods \(.metadata.name) -n \(.metadata.namespace)"' | xargs -n 1 bash -c
 
 exit 0
 
-# SIGUSR1-handler
-my_handler() {
-  exit 143; # 128 + 15 -- SIGTERM
-}
-
-# SIGTERM-handler
-term_handler() {
-  exit 143; # 128 + 15 -- SIGTERM
-}
-
-# setup handlers
-# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
-trap 'kill ${!}; my_handler' SIGUSR1
-trap 'kill ${!}; term_handler' SIGTERM
-
-while true
-do
-  tail -f /dev/null & wait ${!}
-done
