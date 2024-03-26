0012ec26cdc93d40ca022e5fc924a2cc7e9e24c5
srinandan <13950006+srinandan@users.noreply.github.com>
2024-03-26 16:03:08 -0700

    update load gen flags

--- build_deploy.sh
+++ build_deploy.sh
@@ -6,6 +6,7 @@ skaffold build -p gcb --default-repo=us-docker.pkg.dev/$PROJECT_ID/images --quie
 # deploy to GKE cluster
 gcloud deploy apply --file=clouddeploy.yaml --region=$LOCATION --project=$PROJECT_ID
 
-gcloud deploy releases create sample-apps-$((RANDOM % 900 + 100)) --project=$PROJECT_ID --build-artifacts=build_result.json --region=$LOCATION --delivery-pipeline=sample-apps-pipeline
+# openssl rand -base64 25 | tr -dc 'a-z0-9' | cut -c1-4
+gcloud deploy releases create sample-apps-$(base64 < /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n 1) --project=$PROJECT_ID --build-artifacts=build_result.json --region=$LOCATION --delivery-pipeline=sample-apps-pipeline
 
 rm build_result.json
